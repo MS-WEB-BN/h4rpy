58d30287949a90d13747e134eef7e53eb8d4f17b
MS-WEB-BN <56588859+MS-WEB-BN@users.noreply.github.com>
2020-09-08 17:21:32 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,5 +3,6 @@
 rm Output/sample.txt
 apt-get install -y aircrack-ng
 apt-get install -y terminator
-mkdir /root/.config && mkdir /root/.config/terminator
-mv config /root/.config/terminator/config
+mkdir /root/.config
+mkdir /root/.config/terminator
+cp config /root/.config/terminator/config
